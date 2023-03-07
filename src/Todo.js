ff1a5cfa1295d8b87e2b841bdc24a2b8d66a6132
Sharjeel Faiq <mrmalik610@gmail.com>
2023-03-08 01:58:25 +0500

    First version of TextMate app completed. Going to deploy it.

--- src/Todo.js
+++ src/Todo.js
@@ -5,33 +5,87 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 import "@fontsource/roboto/400.css";
 
 const Todo = () => {
+  // Set the initial state of the activity input and list of activities
   const [activity, setActivity] = useState("");
   const [listData, setListData] = useState([]);
 
+  // Function to add an activity to the list and local storage
   const addActivity = () => {
     if (activity !== "") {
-      setListData([...listData, activity]);
+      // Create a new list with the current activity added
+      const newListData = [...listData, activity];
+      // Convert the list to a JSON string and store it in local storage
+      const newListDataJson = JSON.stringify(newListData);
+      localStorage.setItem("items", newListDataJson);
+      // Update the state with the new list and reset the activity input
+      setListData(newListData);
     }
-    setActivity(""); // reset the input field after adding the activity
+    setActivity("");
   };
 
+  // Effect to load the list from local storage when the component mounts
+  useEffect(() => {
+    // Get the list from local storage
+    const storedListData = localStorage.getItem("items");
+    if (storedListData) {
+      // Parse the JSON string and set it as the initial list state
+      setListData(JSON.parse(storedListData));
+    }
+  }, []);
+
+  // Function to remove an activity from the list and local storage
   const removeActivity = (index) => {
+    // Create a new list with the activity at the specified index removed
     const newList = [...listData];
     newList.splice(index, 1);
+    // Update the state with the new list and reset the activity input
     setListData(newList);
+    // Convert the new list to a JSON string and store it in local storage
+    const newListJson = JSON.stringify(newList);
+    localStorage.setItem("items", newListJson);
   };
 
+  const deleteAll = () => {
+    setListData([]);
+    localStorage.removeItem("items");
+  };
+
+  // Render the component
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="sm" sx={{ paddingY: 2 }}>
-        <Box textAlign="center" sx={{ bgcolor: "#cfe8fc", height: "auto" }}>
+        <Box
+          textAlign="center"
+          sx={{
+            bgcolor: "#cfe8fc",
+            height: "80vh",
+            borderRadius: "10%",
+            overflow: "auto",
+            "&::-webkit-scrollbar": {
+              width: "0.5em",
+              height: "0.5em",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "rgba(0, 0, 0, 0.2)",
+              borderRadius: "0.2em",
+            },
+            "&:hover::-webkit-scrollbar-thumb": {
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+            },
+            boxShadow: "8px 8px 10px 5px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          {/* The app header */}
           <Typography variant="h3" style={{ textAlign: "center" }} gutterBottom>
             TextMate
           </Typography>
+          {/* The activity input */}
           <TextField
             id="standard-basic"
             label="Add Task"
@@ -40,6 +94,7 @@ const Todo = () => {
             onChange={(e) => setActivity(e.target.value)}
             sx={{ paddingY: 1 }}
           />
+          {/* The add button */}
           <Button
             variant="contained"
             onClick={addActivity}
@@ -47,18 +102,29 @@ const Todo = () => {
           >
             Add
           </Button>
+          {/* The list of activities */}
           {listData.map((item, index) => (
-            <Typography style={{ textAlign: "left", paddingLeft: "10px" }} key={index}>
-              {item}
-              <Button
-                variant="outlined"
+            <Typography
+              style={{ textAlign: "left", paddingLeft: "10px" }}
+              key={index}
+            >
+              {/* The activity text */}
+              {index + 1}. {item}
+              {/* The remove button */}
+              <IconButton
+                aria-label="delete"
                 onClick={() => removeActivity(index)}
                 sx={{ marginY: 2, marginX: 1 }}
+                size="small"
+                color="warning"
               >
-                Done
-              </Button>
+                <DeleteIcon />
+              </IconButton>
             </Typography>
           ))}
+          {listData.length > 0 ? (
+            <Button onClick={deleteAll} color="error" variant="outlined">Delete All</Button>
+          ) : null}
         </Box>
       </Container>
     </React.Fragment>
